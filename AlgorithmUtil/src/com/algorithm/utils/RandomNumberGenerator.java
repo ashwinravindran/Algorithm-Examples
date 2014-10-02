a536b3d7f37dc350ceb0fb8c2b94b6334adfdc64
= <aravindran@cleo.com>
2014-10-02 17:42:02 -0500

    adding random generator

---
+++ AlgorithmUtil/src/com/algorithm/utils/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.algorithm.utils;
+
+/**
+ *
+ * @author ARavindran
+ */
+public class RandomNumberGenerator {
+ 
+    public RandomNumberGenerator() {}
+}
